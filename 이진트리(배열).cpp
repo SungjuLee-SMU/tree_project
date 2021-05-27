e7de9e1e27ea77f72fb2935d5e5a58ce4b2f3a20
채주 이 <skxcv312@naver.com>
2021-05-27 17:07:18 +0900

    데이터 구조 과제

--- 이진트리(배열).cpp
+++ 이진트리(배열).cpp
@@ -4,19 +4,18 @@ using namespace std;
 #define _CRT_SECURE_NO_WARNINGS 
 #define MAXNODE 100 
 
-typedef struct{ 
+typedef struct {
     char Name[50];
     int LChild;
     int RChild;
 } node;
 
 
-
 void CreateTree(node Mytree[MAXNODE]) {
     for (int i = 0; i < MAXNODE; i++) {
         strcpy(Mytree[i].Name, "UnUsed");
-        Mytree[i].LChild=-1;
-        Mytree[i].RChild=-1;
+        Mytree[i].LChild = -1;
+        Mytree[i].RChild = -1;
     }
 
     strcpy(Mytree[0].Name, "±è");
@@ -26,27 +25,27 @@ void CreateTree(node Mytree[MAXNODE]) {
     strcpy(Mytree[1].Name, "ÀÌ");
     Mytree[1].LChild = 3;
     Mytree[1].RChild = 4;
-    
+
     strcpy(Mytree[2].Name, "¹Ú");
     Mytree[2].LChild = 5;
     Mytree[2].RChild = 6;
-    
+
     strcpy(Mytree[3].Name, "ÃÖ");
     Mytree[3].LChild = 7;
     Mytree[3].RChild = -1;
-    
+
     strcpy(Mytree[4].Name, "Á¤");
     Mytree[4].LChild = -1;
     Mytree[4].RChild = -1;
-    
+
     strcpy(Mytree[5].Name, "°­");
     Mytree[5].LChild = -1;
     Mytree[5].RChild = -1;
-    
+
     strcpy(Mytree[6].Name, "Á¶");
     Mytree[5].LChild = -1;
     Mytree[5].RChild = -1;
-    
+
     strcpy(Mytree[7].Name, "À±");
     Mytree[5].LChild = -1;
     Mytree[5].RChild = -1;
@@ -54,7 +53,7 @@ void CreateTree(node Mytree[MAXNODE]) {
 
 
 void Traverse(int i, node Mytree[MAXNODE]) {
-    if (strcmp(Mytree[i].Name, "UnUsed") != 0){
+    if (strcmp(Mytree[i].Name, "UnUsed") != 0) {
         cout << Mytree[i].Name << endl;
         if (Mytree[i].LChild != -1) {
             Traverse(Mytree[i].LChild, Mytree);
@@ -65,9 +64,36 @@ void Traverse(int i, node Mytree[MAXNODE]) {
     }
 }
 
+
+bool search(node Mytree[MAXNODE], char ch_name) {
+    int i = 0;
+    while (strcmp(Mytree[i].Name, "UnUsed") != 0) {
+        if (*Mytree[i].Name == ch_name) {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+void Destroy(node* Mytree) {
+    for (int i = 0; i < MAXNODE; i++) {
+        strcpy(Mytree[i].Name, "UnUsed");
+        Mytree[i].LChild = -1;
+        Mytree[i].RChild = -1;
+    }
+}
+
 int main() {
     node Mytree[MAXNODE];
     CreateTree(Mytree);
+    Traverse(0, Mytree);
+
+    //search True = 1 False = 0
+    cout << search(Mytree, 'À±');
+    //Destroy 
+    Destroy(&Mytree[0]);
+
     Traverse(0, Mytree);
 
     return 0;
