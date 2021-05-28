0d53a4fd86201a74a50e3151a9e4f59da0c3c83d
Heo-Hyunjung <82317735+Heo-Hyunjung@users.noreply.github.com>
2021-05-28 15:33:57 +0900

    Update 이진탐색트리(우선순위큐).cpp
    202021039 허현정 우선순위 큐 완료

--- 이진탐색트리(우선순위큐).cpp
+++ 이진탐색트리(우선순위큐).cpp
@@ -1,16 +1,74 @@
-#include "¿Ã¡¯≈Ωªˆ∆Æ∏Æ ∆˜¿Œ≈Õ.h"
-#define SN 10;
+#include <iostream>
+using namespace std;
+
+typedef struct node {
+	int key;
+	int Data;
+	node* LChild;
+	node* RChild;
+}node;
+
+typedef struct Queue{
+	int len;
+	node* Root;
+}Queue;
+typedef node* Nptr;
+typedef Queue* Qptr;
+
+void PreOrder(Nptr T) {
+	if (T != NULL) {
+		cout << T->Data << endl;
+		PreOrder(T->LChild);
+		PreOrder(T->RChild);
+	}
+}
+
+void SuccessorCopy(Nptr& T, int& Key) {
+	if (T->LChild == NULL) {
+		Key = T->Data;
+		Nptr Temp = T;
+		T = T->RChild;
+		delete Temp;
+	}
+	else
+		SuccessorCopy(T->LChild, Key);
+}
 
 
-
-Nptr Insert(Nptr T, int Key, int Data)
-{
+void Delete(Nptr& T, int Key) {
 	if (T == NULL)
-	{
+		printf("No Record with Such Key");
+	else if (T->key > Key)
+		Delete(T->LChild, Key);
+	else if (T->key < Key)
+		Delete(T->RChild, Key);
+	else if (T->key == Key) {
+		if ((T->LChild == NULL) && (T->RChild == NULL)) {
+			Nptr Temp = T;
+			T = NULL;
+			delete Temp;
+		}
+		else if (T->LChild == NULL) {
+			Nptr Temp = T;
+			T = T->RChild;
+			delete Temp;
+		}
+		else if (T->RChild == NULL) {
+			Nptr Temp = T;
+			T = T->LChild;
+			delete Temp;
+		}
+		else
+			SuccessorCopy(T->RChild, T->key);
+	}
+}
+
+Nptr Insert(Nptr T, int Key, int Data) {
+	if (T == NULL) {
 		T = new node;
 		T->key = Key;
 		T->Data = Data;
-		T->LChild = NULL; 
+		T->LChild = NULL;
 		T->RChild = NULL;
 	}
 	else if (T->key > Key)
@@ -20,61 +78,92 @@ Nptr Insert(Nptr T, int Key, int Data)
 	return T;
 }
 
+int LengthQueue(Nptr T) {
+	int count = 0;
+	if (T != NULL) {
+		count++;
+		cout << T->Data << endl;
+		PreOrder(T->LChild);
+		PreOrder(T->RChild);
+	}
+	return count;
+}
 
-
-Qptr MakePriorityQueue() {
-
-	PriorityQueue *PQ;
-	PQ = new PriorityQueue;
-	PQ->Length = 0;
+Qptr Create() {
+	Queue* PQ;
+	PQ = new Queue;
+	PQ->len = 0;
 	PQ->Root = NULL;
-
 	return PQ;
 }
 void Enqueue(Qptr PQ, int key, int Data) {
-	if (PQ->Length == 0) {
+	if (PQ->len == 0) {
 		PQ->Root = Insert(PQ->Root, key, Data);
-		PQ->Length++;
+		PQ->len++;
 	}
-	else Insert(PQ->Root, key, Data);
+	else {
+		Insert(PQ->Root, key, Data);
+		PQ->len++;
+	}
 }
 
 Nptr MinTree(Nptr T) {
 	Nptr temp = T;
 	while (temp->LChild != NULL)
 		temp = temp->LChild;
-
 	return temp;
 
 }
 
-int Dequeue(Nptr& T) {
-	int min = 0;
-	min = MinTree(T)->key;
-	Delete(T, min);
-	return (T)->Data;
+int Dequeue(Qptr PQ) {
+	Nptr min;
+	int tempKey = 0;
+	int tempData = 0;
+
+	min = MinTree(PQ->Root);
+	tempKey = min->key;
+	tempData = min->Data;
+	Delete(PQ->Root, min->key);
+	PQ->len--;
+	return tempData;
 }
 
 int main() {
-	PriorityQueue* PQ=NULL;
-	int Skey[10] = {6,4,8,3,5,7,9,1,2,10};
-	int SData[10] = {60,40,80,30,50,70,90,10,20,100};
+	Queue* PQ = NULL;
+	int Skey[10] = { 6,4,8,3,5,7,9,1,2,10 };
+	int SData[10] = { 60,40,80,30,50,70,90,10,20,100 };
 
-	PQ = MakePriorityQueue();
+	PQ = Create();
 
-	Enqueue(PQ, 6,60);
-	Enqueue(PQ, 4,40);
-	Enqueue(PQ, 8,80);
-	Enqueue(PQ, 3,30);
-	Enqueue(PQ, 5,50);
-	Enqueue(PQ, 7,70);
-	Enqueue(PQ, 9,90);
-	Enqueue(PQ, 1,10);
-	Enqueue(PQ, 2,20);
-	Enqueue(PQ, 10,100);
-//	cout << Dequeue(PQ->Root);
-//	cout << Dequeue(PQ->Root);
+	Enqueue(PQ, 6, 60);
+	Enqueue(PQ, 4, 40);
+	Enqueue(PQ, 8, 80);
+	Enqueue(PQ, 3, 30);
+	Enqueue(PQ, 5, 50);
+	Enqueue(PQ, 7, 70);
+	Enqueue(PQ, 9, 90);
+	Enqueue(PQ, 1, 20000);
+	Enqueue(PQ, 2, 30000);
+	Enqueue(PQ, 10, 100);
 
+	cout << "√Ä√º√Ä¬ß¬º√∏√à¬∏ " << endl;
+	PreOrder(PQ->Root);
+	cout << endl;
+
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+	cout << Dequeue(PQ) << endl;
+
+	cout << "√Ä√º√Ä¬ß¬º√∏√à¬∏ " << endl;
+	PreOrder(PQ->Root);
+	cout << endl;
 
 	return 0;
 }
